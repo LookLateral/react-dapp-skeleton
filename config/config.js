e13354dd1f2b41da1132492d1824264cf64d5860
digibaraka <digiguru@implicitsecurity.com>
2019-02-27 08:41:08 -0800

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    stripe_test_secret_key: process.env.STRIPE_PW || "pk_test_Rcvq9IbB69oZewksljuOumTI",
+    stripe_test_api_key: process.env.STRIPE_API_KEY || "pk_test_Rcvq9IbB69oZewksljuOumTI",
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "UparkH77@1209#v9z3rp74vk",
+    mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 'mongodb://mern:v9z3rp74vk@ds235243.mlab.com:35243/mern'
+  }
+  
+  export default config
