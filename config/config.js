82e889320c225d92789b539d05d4347aa3a12b3e
frazlook <imamu.frazier@looklateral.com>
2019-02-27 09:24:31 -0800

    Update Stripe config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    stripe_test_secret_key: process.env.STRIPE_PW || "pk_test_Rcvq9IbB69oZewksljuOumTI",
-    stripe_test_api_key: process.env.STRIPE_API_KEY || "pk_test_Rcvq9IbB69oZewksljuOumTI",
+    stripe_test_secret_key: process.env.STRIPE_PW || "pk_test_yPaUf3r449i7Kg6HMUr71bLm",
+    stripe_test_api_key: process.env.STRIPE_API_KEY || "pk_test_yPaUf3r449i7Kg6HMUr71bLm",
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "UparkH77@1209#v9z3rp74vk",
